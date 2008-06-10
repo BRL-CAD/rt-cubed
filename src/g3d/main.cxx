04fd274af013d978a74947794a570af06b9e4814
Manuel Montecelo <manuel.montezelo@gmail.com>
2008-06-10 10:35:19 +0000

    Initial patch for the 3D Geometry Editor, the entry point of the application.

---
+++ src/g3d/main.cxx
@@ -0,0 +1,45 @@
+/*
+ * BRL-CAD
+ *
+ * Copyright (c) 2008 United States Government as represented by the
+ * U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+
+/** @file main.cxx
+ *
+ * @brief
+ *	Entry point of the 3D Geometry Editor (g3d).
+ */
+
+#include "Application.h"
+
+int main(int argc, char* argv[])
+{
+        Application app;
+	app.initialize();
+	app.run();
+
+        return 0;
+}
+
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
