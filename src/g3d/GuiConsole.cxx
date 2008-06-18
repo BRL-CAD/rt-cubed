164a9193922c2810ce2655b8ee4e8022703096b1
Christopher Sean Morrison <brlcad@gmail.com>
2008-06-18 05:40:41 +0000

    break out the History class into its own file, plus some comment cleanup

--- src/g3d/GuiConsole.cxx
+++ src/g3d/GuiConsole.cxx
@@ -1,8 +1,8 @@
-/*
+/*                  G U I C O N S O L E . C X X
  * BRL-CAD
  *
- * Copyright (c) 2008 United States Government as represented by the
- * U.S. Army Research Laboratory.
+ * Copyright (c) 2008 United States Government as represented by
+ * the U.S. Army Research Laboratory.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
@@ -17,7 +17,6 @@
  * License along with this file; see the file named COPYING for more
  * information.
  */
-
 /** @file GuiConsole.cxx
  *
  * @author Manuel A. Fernandez Montecelo <mafm@users.sourceforge.net>
@@ -26,6 +25,8 @@
  *	Implementation of the GUI Console class of 3D Geometry Editor
  *	(g3d), along with some internal classes.
  */
+
+//#include "common.h"
 
 #include <OIS/OISKeyboard.h>
 #include <OGRE/OgreRenderWindow.h>
@@ -40,98 +41,23 @@
 #include "Logger.h"
 
 #include "GuiConsole.h"
+#include "History.h"
+
 
 using namespace std;
 
 
-/** @brief Implements History service for the Console
- *
- * @author Manuel A. Fernandez Montecelo <mafm@users.sourceforge.net>
- *
- * The class store the commands the user has entered into the
- * entrybox/prompt of the console window.  It keeps all of them in
- * memory (it's not supposed to represent huge amounts), in sequential
- * order.
- */
-class History {
-public:
-  /** Default constructor */
-  History() : _index(0)
-    { }
-
-  /** Insert a new string, the last one typed */
-  void insert(const char* str)
-    {
-      string s(str);
-      if (s.length() != 0) {
-	_lines.push_back(s);
-	_index = _lines.size();
-	//cout << "History: inserted: [" << _index << "] " << s << endl;
-      }
-    }
-
-  /** Get a string from the history */
-  string getByIndex(size_t i)
-    {
-      if (i < _lines.size()) {
-	// index is in proper range (and _lines not empty, since 0<=i<lines.size())
-	//cout << "History: getByIndex[" << _index << "]: " << _lines[i] << endl;
-	return _lines[i];
-      } else {
-	// index out of range -- return empty string
-	//cout << "History: getByIndex: _lines empty or index out of range, returning empty string" << endl;
-	return string("");
-      }
-    }
-
-  /** Return the next command from history (empty for the "current" one) */
-  string getNext()
-    {
-      if (_index < (_lines.size() - 1)) {
-	_index++;
-	//cout << "History: getNext: [" << _index << "] " << getByIndex(_index) << endl;
-	return getByIndex(_index);
-      } else {
-	// when "returning" from history (we past the most recent
-	// typed), the prompt is cleared
-	return string("");
-      }
-    }
-
-  /** Return the previous command from history, remains in the oldest
-   * if the user continues to press the key */
-  string getPrev()
-    {
-      if (_index > 0)
-	_index--;
-
-      //cout << "History: getPrev: [" << _index << "] " << getByIndex(_index) << endl;
-      return getByIndex(_index);
-    }
-
-private:
-  /** Array of strings to store our history */
-  vector<string> _lines;
-  /** Pointer to the current line */
-  size_t _index;
-};
-
-
-/** @brief
- *
- * @author Manuel A. Fernandez Montecelo <mafm@users.sourceforge.net>
- *
- *	Internal class for [OGRE] Window Resized events, so the
- *	Console accomodates to the new size of the render window
- *	automatically.
+/**
+ * Internal class for [OGRE] Window Resized events, so the Console
+ * accomodates to the new size of the render window automatically.
  */
 class WindowResizedListener : public Ogre::WindowEventListener
 {
 public:
-  /** Default constructor
+  /**
+   * Default constructor
    *
-   * @param guiConsole The GUI Console that this listener is related
-   * to
+   * @param guiConsole The GUI Console that this listener relates to
    */
   WindowResizedListener(GuiConsole& guiConsole) : _guiConsole(guiConsole) { }
 
---
+++ src/g3d/History.h
@@ -0,0 +1,97 @@
+/*
+ *
+ * @author Manuel A. Fernandez Montecelo <mafm@users.sourceforge.net>
+ *
+ * @brief
+ *	Implementation of the GUI Console class of 3D Geometry Editor
+ *	(g3d), along with some internal classes.
+ */
+
+//#include "common.h"
+
+#include <string>
+
+using namespace std;
+
+
+/**
+ * @brief Implements History service for the Console
+ *
+ * @author Manuel A. Fernandez Montecelo <mafm@users.sourceforge.net>
+ *
+ * The class store the commands the user has entered into the
+ * entrybox/prompt of the console window.  It keeps all of them in
+ * memory (it's not supposed to represent huge amounts), in sequential
+ * order.
+ */
+class History {
+public:
+  /** Default constructor */
+  History() : _index(0)
+    { }
+
+  /** Insert a new string, the last one typed */
+  void insert(const char* str)
+    {
+      string s(str);
+      if (s.length() != 0) {
+	_lines.push_back(s);
+	_index = _lines.size();
+	//cout << "History: inserted: [" << _index << "] " << s << endl;
+      }
+    }
+
+  /** Get a string from the history */
+  string getByIndex(size_t i)
+    {
+      if (i < _lines.size()) {
+	// index is in proper range (and _lines not empty, since 0<=i<lines.size())
+	//cout << "History: getByIndex[" << _index << "]: " << _lines[i] << endl;
+	return _lines[i];
+      } else {
+	// index out of range -- return empty string
+	//cout << "History: getByIndex: _lines empty or index out of range, returning empty string" << endl;
+	return string("");
+      }
+    }
+
+  /** Return the next command from history (empty for the "current" one) */
+  string getNext()
+    {
+      if (_index < (_lines.size() - 1)) {
+	_index++;
+	//cout << "History: getNext: [" << _index << "] " << getByIndex(_index) << endl;
+	return getByIndex(_index);
+      } else {
+	// when "returning" from history (we past the most recent
+	// typed), the prompt is cleared
+	return string("");
+      }
+    }
+
+  /** Return the previous command from history, remains in the oldest
+   * if the user continues to press the key */
+  string getPrev()
+    {
+      if (_index > 0)
+	_index--;
+
+      //cout << "History: getPrev: [" << _index << "] " << getByIndex(_index) << endl;
+      return getByIndex(_index);
+    }
+
+private:
+  /** Array of strings to store our history */
+  vector<string> _lines;
+  /** Pointer to the current line */
+  size_t _index;
+};
+
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
