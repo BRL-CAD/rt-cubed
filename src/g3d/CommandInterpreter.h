27475fef6af7e70f0d31a33aab5a87e06bdb9398
Manuel Montecelo <manuel.montezelo@gmail.com>
2008-08-15 16:00:55 +0000

    Movind Command base class to a separate file, adding a new command to start to use libged (forgot to add this file in previous commit)

--- src/g3d/CommandInterpreter.h
+++ src/g3d/CommandInterpreter.h
@@ -31,74 +31,11 @@
 
 
 #include <string>
-#include <vector>
 #include <map>
 
 
-/**
- * @brief Output for a command
- *
- * @author Manuel A. Fernandez Montecelo <mafm@users.sourceforge.net>
- */
-class CommandOutput
-{
-public:
-  /** Append given string to the output of a command */
-  void appendLine(const std::string& line);
-  /** Get the output of a command */
-  const std::string& getOutput() const;
-
-private:
-  /** The complete output */
-  std::string _output;
-};
-
-
-/**
- * @brief Command abstract class
- *
- * @author Manuel A. Fernandez Montecelo <mafm@users.sourceforge.net>
- *
- * A base class of a command, derived classes need to define the
- * execute() method, and also create a constructor to call the
- * constructor of the base class with the appropriate parameters
- * (constructors themselves cannot be virtual).
- */
-class Command
-{
-public:
-  /** Constructor with some basics needed when creating any
-   * command. */
-  Command(const std::string& name,
-	  const std::string& shortDescr,
-	  const std::string& extraDescr);
-  /** Default destructor */
-  virtual ~Command() { }
-
-  /** Execute the command (to be implemented by the real commands) */
-  virtual void execute(std::vector<std::string>& args, CommandOutput& output) = 0;
-
-  /** The name of the command */
-  const std::string& getName() const;
-  /** Get a one-line description of the command */
-  const std::string& getShortDescription() const;
-  /** Get a extra description of the command */
-  const std::string& getExtraDescription() const;
-  /** Get the name of the arguments */
-  const std::vector<std::string>& getArgNames() const;
-  /** Get the syntax */
-  std::string getSyntax() const;
-
-protected:
-  /** The name of the command */
-  std::string _name;
-  /** Description, one line only if possible */
-  std::string _shortDescription;
-  /** Extra description */
-  std::string _extraDescription;
-  /** The names of the arguments */
-  std::vector<std::string> _argNames;
-};
+class Command;
+class CommandOutput;
 
 
 /**
