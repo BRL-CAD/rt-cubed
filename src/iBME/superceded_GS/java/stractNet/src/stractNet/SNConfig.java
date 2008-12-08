ad920d3bbbd25cf30fe543d6cd037244eb4fbf4e
David Loman <dloman77@gmail.com>
2008-12-08 14:07:02 +0000

    Moving GS and GE components in adhere to rt^3 guidelines.

--- src/iBME/superceded_GS/java/stractNet/src/stractNet/SNConfig.java
+++
@@ -1,9 +0,0 @@
-package stractNet;
-
-public class SNConfig extends SNRoot {
-
-	public SNConfig(String Name) {
-		super(Name);
-	}
-
-}
--- src/iBME/superceded_GS/java/stractThread/src/stractThread/STPauseable.java
+++
@@ -1,9 +0,0 @@
-package stractThread;
-
-public interface STPauseable extends STRunnable {
-
-	public void enable();
-	public void disable();
-	public boolean isEnabled();
-
-}
