10a0ef5ad809c4c7e77e24f9247218503aa817ce
David Loman <dloman77@gmail.com>
2009-07-07 19:41:58 +0000

    Header and Footer CMake banners.

---
+++ cmake/PrintHeader.cmake
@@ -0,0 +1,35 @@
+#########################################################################
+#
+#	 BRL-CAD
+#	
+#	 Copyright (c) 1997-2009 United States Government as represented by
+#	 the U.S. Army Research Laboratory.
+#	
+#	 This library is free software; you can redistribute it and/or
+#	 modify it under the terms of the GNU Lesser General Public License
+#	 version 2.1 as published by the Free Software Foundation.
+#	
+#	 This library is distributed in the hope that it will be useful, but
+#	 WITHOUT ANY WARRANTY; without even the implied warranty of
+#	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#	 Lesser General Public License for more details.
+#	
+#	 You should have received a copy of the GNU Lesser General Public
+#	 License along with this file; see the file named COPYING for more
+#	 information.
+#	
+#########################################################################
+# 	@file /CMakeModules/PrintHeader.cmake
+#
+#	CMakeModule that prints the Build System Header.
+#
+#	$Revision: 361 $
+#	$Author: Claymore1977 $
+#
+#########################################################################
+
+MESSAGE(STATUS "\n")
+MESSAGE(STATUS "################################################")
+MESSAGE(STATUS "##        rt^3 Pre-Build System (CMake)       ##")
+MESSAGE(STATUS "################################################\n")
+
