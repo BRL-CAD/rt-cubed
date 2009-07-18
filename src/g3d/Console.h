77e56d8f54563c69d328c684f629dc989b6dfd96
Benjamin Saunders <ben.e.saunders@gmail.com>
2009-07-18 01:30:18 +0000

    Made the console its own widget.

---
+++ src/g3d/Console.h
@@ -0,0 +1,58 @@
+/*                  M A I N W I N D O W . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2008-2009 United States Government as represented by the
+ * U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+
+/** @file OgreGLWidget.h
+ *
+ * @author Benjamin Saunders <ralith@users.sourceforge.net>
+ *
+ * @brief 
+ *	Header for the Console widget
+ */
+
+#ifndef __G3D_CONSOLE_H__
+#define __G3D_CONSOLE_H__
+
+#include <QtGui>
+
+class Console : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Console(QWidget *parent = NULL);
+
+private:
+    QVBoxLayout *layout;
+    
+    QLineEdit *entry;
+    QLabel *output;
+};
+
+#endif
+
+/*
+ * Local Variables:
+ * mode: C++
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
