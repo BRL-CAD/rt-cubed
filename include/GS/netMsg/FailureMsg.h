dd5ce052610298aa35ee0ec13f4787e5b5b61c83
David Loman <dloman77@gmail.com>
2009-11-25 16:48:03 +0000

    Add Generic*Msg classes for extensibility

---
+++ include/GS/netMsg/FailureMsg.h
@@ -0,0 +1,62 @@
+/*         F A I L U R E M S G . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2009 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file FailureMsg.h
+ *
+ * Brief description
+ *
+ */
+
+#ifndef __FAILUREMSG_H__
+#define __FAILUREMSG_H__
+
+#include "iBME/iBMECommon.h"
+#include "GS/netMsg/GenericOneByteMsg.h"
+
+
+class FailureMsg : public GenericOneByteMsg
+{
+
+public:
+
+  //Normal Constructor
+  FailureMsg(quint8 failureCode);
+
+  //Reply Constructor
+  FailureMsg(NetMsg* msg, quint8 failureCode);
+
+  //Deserializing Constructors
+  FailureMsg(QDataStream* ds);
+  
+  /*
+   *Getters n Setters
+   */
+  quint8 getFailureCode();
+
+};
+
+#endif
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
---
+++ src/GS/netMsg/GenericTwoBytesMsg.cxx
@@ -0,0 +1,95 @@
+/*             G E N E R I C T W O B Y T E S M S G . C X X
+ * BRL-CAD
+ *
+ * Copyright (c) 2009 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file GenericTwoBytesMsg.cxx
+ *
+ * Brief description
+ *
+ */
+
+#include "GS/netMsg/GenericTwoBytesMsg.h"
+#include <sstream>
+
+//Normal Constructor
+GenericTwoBytesMsg::GenericTwoBytesMsg(quint32 type, quint16 b):
+  NetMsg(type), data(b)
+{
+}
+
+  //Reply Constructor
+GenericTwoBytesMsg::GenericTwoBytesMsg(quint32 type, NetMsg* msg, quint16 b):
+  NetMsg(type, msg), data(b)
+{
+}
+
+//Deserializing Constructors
+GenericTwoBytesMsg::GenericTwoBytesMsg(QDataStream* ds):
+NetMsg(ds)
+{
+   *ds >> this->data;
+}
+
+//Destructor
+GenericTwoBytesMsg::~GenericTwoBytesMsg()
+{
+}
+
+bool GenericTwoBytesMsg::_serialize(QDataStream* ds)
+{
+  *ds << this->data;
+  return true;
+}
+
+QString GenericTwoBytesMsg::toString() 
+{
+  QString out;
+
+  out.append(NetMsg::toString());
+  out.append("\t data: '");
+  out.append(QString::number(this->data));
+  out.append("'");
+
+  return out; 
+}
+
+
+bool
+GenericTwoBytesMsg::_equals(NetMsg& msg) 
+{
+  GenericTwoBytesMsg& gmsg = (GenericTwoBytesMsg&) msg;
+
+  if (this->getData() != gmsg.getData()) {
+    return false;
+  }
+
+  return true;
+}
+
+ /*
+ *Getters n Setters
+ */
+quint16 GenericTwoBytesMsg::getData() {return this->data;}
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
