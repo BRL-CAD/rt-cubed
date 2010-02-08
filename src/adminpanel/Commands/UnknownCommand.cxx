1bbede1a1efeb97ce78b6e75eddcd0ba0f92ce86
David Loman <dloman77@gmail.com>
2010-02-08 18:37:39 +0000

    Stub in UnknownCommand

---
+++ src/adminpanel/Commands/UnknownCommand.cxx
@@ -0,0 +1,45 @@
+/*                 U N K N O W N C O M M A N D . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2009 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file UnknownCommand.h
+ *
+ * Brief description
+ *
+ */
+#include "UnknownCommand.h"
+
+UnknownCommand::UnknownCommand()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+UnknownCommand::~UnknownCommand()
+{
+	// TODO Auto-generated destructor stub
+}
+
+
+// Local Variables:
+// mode: C++
+// tab-width: 8
+// c-basic-offset: 2
+// indent-tabs-mode: t
+// End:
+// ex: shiftwidth=2 tabstop=8
---
+++ src/adminpanel/Commands/UnknownCommand.h
@@ -0,0 +1,46 @@
+/*                 U N K N O W N C O M M A N D . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2009 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file UnknownCommand.h
+ *
+ * Brief description
+ *
+ */
+
+#ifndef UNKNOWNCOMMAND_H_
+#define UNKNOWNCOMMAND_H_
+
+#include "GS/Jobs/AbstractJob.h"
+
+class UnknownCommand: public AbstractJob
+{
+public:
+	UnknownCommand();
+	virtual ~UnknownCommand();
+};
+
+#endif /* UNKNOWNCOMMAND_H_ */
+
+// Local Variables:
+// mode: C++
+// tab-width: 8
+// c-basic-offset: 2
+// indent-tabs-mode: t
+// End:
+// ex: shiftwidth=2 tabstop=8
