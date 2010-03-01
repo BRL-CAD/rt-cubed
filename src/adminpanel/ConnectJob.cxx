5fd92820524c9763f82d6601c43b2d8bb6d46bca
David Loman <dloman77@gmail.com>
2010-03-01 20:13:19 +0000

    Stub in first of many AbstractJob subclasses.

---
+++ src/adminpanel/ConnectJob.cxx
@@ -0,0 +1,42 @@
+/*                  C O N N E C T J O B . C X X
+ * BRL-CAD
+ *
+ * Copyright (c) 2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file ConnectJob.cxx
+ *
+ * Brief description
+ *
+ */
+
+#include "ConnectJob.h"
+
+ConnectJob::ConnectJob()
+{
+}
+ConnectJob::~ConnectJob()
+{
+}
+
+// Local Variables:
+// tab-width: 8
+// mode: C++
+// c-basic-offset: 4
+// indent-tabs-mode: t
+// c-file-style: "stroustrup"
+// End:
+// ex: shiftwidth=4 tabstop=8
