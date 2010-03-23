1def08a911a2d43536a9061e124c1267c97891b1
David Loman <dloman77@gmail.com>
2010-03-23 16:31:42 +0000

    Modify test to utilize new wrapper.

--- tests/GS/libNetwork/netPortalManagerTest.cxx
+++ tests/GS/libNetwork/netPortalManagerTest.cxx
@@ -1,4 +1,3 @@
-
 #include "GS/libNetwork/NetMsg.h"
 #include "GS/libNetwork/NetMsgFactory.h"
 #include "GS/libNetwork/GenericOneStringMsg.h"
@@ -18,6 +17,9 @@
 #include "GS/libNetwork/GeometryChunkMsg.h"
 
 #include "GS/libNetwork/NetPortalManager.h"
+#include "GS/libNetwork/FailureMsg.h"
+
+#include "NetPortalManagerTester.h"
 
 #include <QCoreApplication>
 
@@ -31,23 +33,31 @@
 
 int main(int argc, char* argv[])
 {
-    QCoreApplication coreApp(argc, argv);
-
-    NetPortalManager netMan01("Gomer");
-    NetPortalManager netMan02("Kiaser");
-
+    //Setup
     QHostAddress netMan01Addy = QHostAddress::LocalHost;
     quint16 netMan01Port = 6000;
 
     QHostAddress netMan02Addy = QHostAddress::LocalHost;
     quint16 netMan02Port = 7000;
 
+    //Core app
+    QCoreApplication coreApp(argc, argv);
+
+    NetPortalManager netMan01("Gomer");
+    NetPortalManager netMan02("Kiaser");
+
+    NetPortalManagerTester npmt01(&netMan01);
+    NetPortalManagerTester npmt02(&netMan02);
+
     netMan01.listen(netMan01Addy, netMan01Port);
     netMan02.listen(netMan02Addy, netMan02Port);
 
     //Connect to netMan02
-    NetPortal* np01 = netMan01.getNewPortal();
+    NetPortal* np01 = npmt01.getNewPortal();
+    NetPortal* np02 = npmt02.getNewPortal();
     np01->connectToHost(netMan02Addy, netMan02Port);
+
+    np01->quickSend(12345677);
 
     return coreApp.exec();
 }
