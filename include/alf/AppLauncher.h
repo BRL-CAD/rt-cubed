de44ea77aefa74b8571d65b154696760c29c492f
David Loman <dloman77@gmail.com>
2010-04-05 19:24:29 +0000

    Stub in the basics for Application Launch Framework.

---
+++ include/alf/AppLauncher.h
@@ -0,0 +1,51 @@
+/*                A P P L A U N C H E R . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file AppLauncher.ch
+ *
+ * Launchpad for applications.
+ *
+ */
+
+#ifndef __APPLAUNCHER_H__
+#define __APPLAUNCHER_H__
+
+#include "GS/GSCommon.h"
+#include <QString>
+#include <QUuid>
+
+class AppLauncher {
+
+public:
+	AppLauncher();
+	virtual ~AppLauncher();
+
+private:
+
+};
+
+#endif /* __APPLAUNCHER_H__ */
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
---
+++ src/alf/AppLauncher.cxx
@@ -0,0 +1,46 @@
+/*                A P P L A U N C H E R . C X X
+ * BRL-CAD
+ *
+ * Copyright (c) 2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file AppLauncher.cxx
+ *
+ * Launchpad for applications.
+ *
+ */
+
+#include <iostream>
+#include "alf/AppLauncher.h"
+
+int main(int argc, char* argv[]) {
+	std::cout << "App Launcher";
+	return 0;
+}
+
+AppLauncher::AppLauncher() {
+}
+
+AppLauncher::~AppLauncher() {
+}
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
