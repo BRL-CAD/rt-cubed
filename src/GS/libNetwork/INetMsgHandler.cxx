3f04043a75ad3d6ca1f96c6e8eb4c3bb7e06d43a
David Loman <dloman77@gmail.com>
2010-04-05 13:39:53 +0000

    Stub in a NetMsg handler interface and a NetMsg subscription system.  Implementation to follow.

---
+++ src/GS/libNetwork/INetMsgHandler.cxx
@@ -0,0 +1,45 @@
+/*              I N E T M S G H A N D L E R . C X X
+ * BRL-CAD
+ *
+ * Copyright (c) 2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file INetMsgHandler.cxx
+ *
+ * Interface.  Any class needing to handle incoming
+ * NetMsgs and be registered to the NetMsgSubscriberRegistry
+ * must implement this interface.
+ *
+ */
+
+#include "GS/libNetwork/INetMsgHandler.h"
+
+INetMsgHandler::INetMsgHandler() {
+	// TODO Auto-generated constructor stub
+
+}
+
+INetMsgHandler::~INetMsgHandler() {
+	// TODO Auto-generated destructor stub
+}
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
---
+++ src/GS/libNetwork/NetMsgSubscriberRegistry.cxx
@@ -0,0 +1,45 @@
+/*        N E T M S G S U B S C R I B E R R E G I S T R Y . C X X
+ * BRL-CAD
+ *
+ * Copyright (c) 2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file NetMsgSubscriberRegistry.cxx
+ *
+ * Provides a means for any class implementing the
+ * INetMsgHandler interface to be registered onto
+ * the NetMsg distribution lists.
+ *
+ */
+
+#include "GS/libNetwork/NetMsgSubscriberRegistry.h"
+
+NetMsgSubscriberRegistry::NetMsgSubscriberRegistry() {
+	// TODO Auto-generated constructor stub
+
+}
+
+NetMsgSubscriberRegistry::~NetMsgSubscriberRegistry() {
+	// TODO Auto-generated destructor stub
+}
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
