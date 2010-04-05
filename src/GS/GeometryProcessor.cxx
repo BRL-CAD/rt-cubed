c732cf96756af823840e60754eb4306e0862176d
David Loman <dloman77@gmail.com>
2010-04-05 16:15:10 +0000

    Stub in GeometryProcessor singleton.  Will be the POC for GE calls.

---
+++ src/GS/GeometryProcessor.cxx
@@ -0,0 +1,52 @@
+/*            G E O M E T E R Y P R O C E S S O R . C X X
+ * BRL-CAD
+ *
+ * Copyright (c) 2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file GeometryProcessor.cxx
+ *
+ * Provides a single point of access for
+ * manipulating Geometry. This is the prototype
+ * interface to the Geometry Engine.
+ *
+ */
+
+#include "GS/GeometryProcessor.h"
+#include <QMutexLocker>
+
+GeometryProcessor* GeometryProcessor::pInstance = NULL;
+
+GeometryProcessor::GeometryProcessor() {
+}
+
+GeometryProcessor::~GeometryProcessor() {
+}
+
+GeometryProcessor* GeometryProcessor::getInstance() {
+	if (!GeometryProcessor::pInstance) {
+		pInstance = new GeometryProcessor();
+	}
+	return GeometryProcessor::pInstance;
+}
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
