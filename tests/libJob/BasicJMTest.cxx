4ade3f5f66877d52ca17ba0b24079ef17dab6410
David Loman <dloman77@gmail.com>
2010-04-21 11:08:43 +0000

    Stub in start of basic JobManager test.

---
+++ tests/libJob/BasicJMTest.cxx
@@ -0,0 +1,37 @@
+/*                 B A S I C J M T E S T . C X X
+ * BRLCAD
+ *
+ * Copyright (c) 2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file BasicJMTest.cxx
+ *
+ * Brief description
+ *
+ */
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
