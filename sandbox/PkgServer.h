3f4e29eb1c992dbda6039751eb71b581cf2e6a2f
David Loman <dloman77@gmail.com>
2010-08-10 18:23:36 +0000

    Stub in some base classess for a cxx layer of libpkg

---
+++ sandbox/PkgServer.h
@@ -0,0 +1,53 @@
+/*                    P K G S E V E R . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2004-2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file PkgServer.h
+ *
+ *
+ */
+
+#ifndef __PKGSERVER_H__
+#define __PKGSERVER_H__
+
+class PkgServer
+{
+public:
+    PkgServer();
+    virtual ~PkgServer();
+
+
+
+private:
+    quint32 listenFD;
+
+};
+
+#endif /* __PKGSERVER_H__ */
+
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
+
