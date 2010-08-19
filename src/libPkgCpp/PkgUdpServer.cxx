66ba1c9e00688be118061cd048e502098533544a
David Loman <dloman77@gmail.com>
2010-08-19 19:18:50 +0000

    Stub in tcp and udp subclasses of PkgServer in prep for inheritance heirarchy change.

---
+++ src/libPkgCpp/PkgUdpServer.cxx
@@ -0,0 +1,48 @@
+/*                P K G U D P S E R V E R . C X X
+ * BRL-CAD
+ *
+ * Copyright (c) 2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file PkgUdpServer.cxx
+ * PkgUdpServer.cxx
+ *
+ *  Created on: Aug 19, 2010
+ *      Author: dloman
+ */
+
+#include "PkgUdpServer.h"
+
+PkgUdpServer::PkgUdpServer(pkg_switch callbacks[]) :
+PkgServer(PKGCPP_PROTO_UDP, callbacks)
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+PkgUdpServer::~PkgUdpServer()
+{
+  // TODO Auto-generated destructor stub
+}
+
+// Local Variables:
+// tab-width: 8
+// mode: C++
+// c-basic-offset: 4
+// indent-tabs-mode: t
+// c-file-style: "stroustrup"
+// End:
+// ex: shiftwidth=4 tabstop=8
---
+++ src/libPkgCpp/PkgUdpServer.h
@@ -0,0 +1,49 @@
+/*                  P K G U D P S E R V E R . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file PkgUdpServer.h
+ * PkgUdpServer.h
+ *
+ *  Created on: Aug 19, 2010
+ *      Author: dloman
+ */
+
+#ifndef __PKGUDPSERVER_H__
+#define __PKGUDPSERVER_H__
+
+#include "PkgServer.h"
+
+class PkgUdpServer : public PkgServer
+{
+public:
+  PkgUdpServer(pkg_switch callbacks[]);
+  virtual ~PkgUdpServer();
+};
+
+#endif /* __PKGUDPSERVER_H__ */
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
