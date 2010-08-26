d1810e235ab4b803d99d9eeb7727804caec5e9d3
David Loman <dloman77@gmail.com>
2010-08-26 15:17:07 +0000

    Stub in a wrapper class for QThread.  Will enable us to track the creation and status of threads much easier.

---
+++ include/GSThread.h
@@ -0,0 +1,49 @@
+/*                      G S T H R E A D . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file GSThread.h
+ *
+ * Brief description
+ *
+ */
+
+#ifndef __GSTHREAD_H__
+#define __GSTHREAD_H__
+
+#include <QtCore/QThread>
+
+class GSThread : public QThread
+{
+public:
+  GSThread();
+  virtual
+  ~GSThread();
+};
+
+#endif /* __GSTHREAD_H__ */
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
