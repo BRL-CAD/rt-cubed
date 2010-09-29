c47886dae9135e0d1612a7821490976ac9aa70b8
David Loman <dloman77@gmail.com>
2010-09-29 11:44:34 +0000

    Make geoserv.cxx parse config for node name and port.

--- src/GS/geoserv.cxx
+++ src/GS/geoserv.cxx
@@ -17,7 +17,7 @@
  * License along with this file; see the file named COPYING for more
  * information.
  */
-/** @file gsmain.cxx
+/** @file geoserv.cxx
  *
  * Brief description
  *
@@ -30,18 +30,20 @@
 #include "libevent.h"
 
 #include <iostream>
+#include <stdlib.h>
 
 #include <QtCore/QString>
 
 int main(int argc, char* argv[])
 {
     std::cout << std::endl << std::endl;
-
     Logger* log = Logger::getInstance();
+    log->logBANNER("geoserv", "GeometryService Config Loader");
+
     Config* c = Config::getInstance();
 
     //TODO Configure system loads stuff here
-    if (c->loadFile("geoserve.config")) {
+    if (c->loadFile("geoserve.config", true)) {
 	return 1;
     }
 
@@ -50,7 +52,34 @@ int main(int argc, char* argv[])
     	localNodename = "DefaultGSNodename";
     }
 
-    log->logBANNER("GSMain", "Booting GeometryService: " + localNodename);
+
+    log->logBANNER("geoserv", "Booting GeometryService: " + localNodename);
+
+    QString sport = c->getConfigValue("ListenPort");
+    if (sport == NULL){
+    	log->logERROR("geoserv", "Config File does not contain a 'ListenPort' parameter");
+    	return 1;
+    }
+    if (sport.length() <= 0){
+    	log->logERROR("geoserv", "Config File contains a 'ListenPort' key, however the value was <= 0.");
+    	return 1;
+    }
+
+    bool ok;
+    quint16 port = sport.toUShort(&ok, 10);
+    if (!ok){
+    	log->logERROR("geoserv", "Config File contains a 'ListenPort' key, however the value failed to parse to a valid number.");
+    	return 1;
+    }
+
+    bool daemon = false;
+
+    GeometryService* gs = new GeometryService(localNodename, port);
+
+    if (daemon){
+    	gs->start();
+    }
+
 
     return 0;
 }
