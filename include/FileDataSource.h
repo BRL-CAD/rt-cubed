a63a984512679e14b7aa9dca9324412f150254fa
David Loman <dloman77@gmail.com>
2010-09-29 18:18:32 +0000

    Stub in FileDataSource class.  Will be the data source used for io with file based repositories

---
+++ include/FileDataSource.h
@@ -0,0 +1,47 @@
+/*                F I L E D A T A S O U R C E . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file FileDataSource.h
+ *
+ * Brief description
+ *
+ */
+
+#ifndef __FILEDATASOURCE_H__
+#define __FILEDATASOURCE_H__
+
+#include "IDataSource.h"
+
+class FileDataSource: public IDataSource {
+public:
+	FileDataSource();
+	virtual ~FileDataSource();
+};
+
+#endif /* __FILEDATASOURCE_H__ */
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
---
+++ src/GS/FileDataSource.cxx
@@ -0,0 +1,42 @@
+/*              F I L E D A T A S O U R C E . C X X
+ * BRL-CAD
+ *
+ * Copyright (c) 2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file FileDataSource.cxx
+ *
+ * Brief description
+ *
+ */
+
+
+#include "FileDataSource.h"
+
+FileDataSource::FileDataSource() {
+}
+
+FileDataSource::~FileDataSource() {
+}
+
+// Local Variables:
+// tab-width: 8
+// mode: C++
+// c-basic-offset: 4
+// indent-tabs-mode: t
+// c-file-style: "stroustrup"
+// End:
+// ex: shiftwidth=4 tabstop=8
