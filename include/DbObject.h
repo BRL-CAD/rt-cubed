30c0f30e6ade36e1c1422af184d73d039cbfd69c
David Loman <dloman77@gmail.com>
2010-09-29 18:02:07 +0000

    Make several libgs headers public.  Need them public for interconnectivity between the libraries.

---
+++ include/DbObject.h
@@ -0,0 +1,46 @@
+/*                      D B O B J E C T . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file DbObject.h
+ * DbObject.h
+ *
+ *  Created on: Sep 29, 2010
+ *      Author: dloman
+ */
+
+#ifndef DBOBJECT_H_
+#define DBOBJECT_H_
+
+class DbObject {
+public:
+	DbObject();
+	virtual ~DbObject();
+};
+
+#endif /* DBOBJECT_H_ */
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
