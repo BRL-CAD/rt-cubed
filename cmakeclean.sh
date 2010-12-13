3366ef011d1b6ab54e6626fff98c55705f99cac9
David Loman <dloman77@gmail.com>
2010-12-13 20:03:40 +0000

    Eliminated Whitespace.

--- cmakeclean.sh
+++ cmakeclean.sh
@@ -1,5 +1,5 @@
  find . -name "CMakeFiles" | xargs rm -fr
-  find . -name "__" | xargs rm -fr
+ find . -name "__" | xargs rm -fr
  find . -name "cmake_install.cmake" | xargs rm
-  find . -name "Makefile" | xargs rm
+ find . -name "Makefile" | xargs rm
  
