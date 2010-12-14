ac8aa5fc8ebafe894c82333c68df9964edfec596
David Loman <dloman77@gmail.com>
2010-12-14 20:43:41 +0000

    Starting work on getting class headers Doxygenized.

--- include/AbstractJob.h
+++ include/AbstractJob.h
@@ -19,8 +19,6 @@
  */
 /** @file AbstractJob.h
  *
- * Brief description
- *
  */
 
 #ifndef __ABSTRACTJOB_H__
@@ -28,23 +26,48 @@
 
 #include <QtCore/Qt>
 
+/** @enum JobStatus
+ * Defines three values relating to the potential status of the job.
+ * @param JOB_NOTSTARTED Job has been created, perhaps submitted for execution,  but not yet executed.
+ * @param JOB_RUNNING Job is in process of being executed.
+ * @param JOB_FINISHED Job has finished executing.
+ *
+ */
 enum JobStatus
 {
 	JOB_NOTSTARTED, JOB_RUNNING, JOB_FINISHED
 };
+
+/** @enum JobResult
+ * Defines three values relating to the result of a finished job.
+ * @param JOB_COMPLETED_NO_ERRORS Job has finished without generating any errors.
+ * @param JOB_COMPLETED_WITH_ERRORS Job has finished, but generated errors.
+ * @param JOB_FAILED  Job did not finish.
+ *
+ */
 enum JobResult
 {
 	JOB_COMPLETED_NO_ERRORS, JOB_COMPLETED_WITH_ERRORS, JOB_FAILED
 };
 
+/** @class AbstractJob
+ * @brief Base implementation for an abstract piece of work to be performed by the JobManager.
+ *
+ * AbstractJob is a virtual class and defines the required enums and parameters needed for subclasses of AbstractJob
+ * to successfully submit and execute their code in the JobManager.
+ *
+ */
 class AbstractJob
 {
 public:
-	AbstractJob();
+
+	/**
+	 * Standard Destructor.  Nothing to see here.
+	 */
 	virtual ~AbstractJob();
 
 	/**
-	 * Submits this job to the JobManager.  (Dangerous, need to remove)
+	 * Submits this job to the JobManager.  //TODO: This is dangerous, need to remove!
 	 */
 	void submit();
 
@@ -64,6 +87,10 @@ public:
 	quint32 getJobId();
 
 protected:
+	/**
+	 * Standard Default Constructor.  Nothing to see here.
+	 */
+	AbstractJob();
 
 	/**
 	 * Mandates subclass implementation of _doJob().  This function provides the actual
@@ -90,11 +117,11 @@ private:
 #endif /* __ABSTRACTJOB_H__ */
 
 /*
- * Local Variables: ***
- * mode: C++ ***
- * tab-width: 8 ***
- * c-basic-offset: 2 ***
- * indent-tabs-mode: t ***
- * End: ***
+ * Local Variables:
+ * mode: C++
+ * tab-width: 8
+ * c-basic-offset: 2
+ * indent-tabs-mode: t
+ * End:
  * ex: shiftwidth=2 tabstop=8
 */
