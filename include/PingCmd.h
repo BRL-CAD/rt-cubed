87db918084070994e6556b14e762085df0bdafa1
David Loman <dloman77@gmail.com>
2010-12-22 14:18:40 +0000

    Implement PingCmd.  Used by GSClient to initiate a ping.

---
+++ include/PingCmd.h
@@ -0,0 +1,54 @@
+/*                       P I N G C M D . H
+ * BRLCAD
+ *
+ * Copyright (c) 2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file PingCmd.h
+ * PingCmd.h
+ *
+ *  Created on: Dec 22, 2010
+ */
+
+#ifndef __PINGCMD_H__
+#define __PINGCMD_H__
+
+#include "AbstractClientCmd.h"
+#include <QtCore/QString>
+
+class PingCmd: public AbstractClientCmd {
+public:
+	PingCmd();
+	virtual ~PingCmd();
+	QString getUsage();
+	QString getHelp();
+
+protected:
+	bool _exec(GSClient* client, QStringList args);
+
+};
+
+#endif /* __PINGCMD_H__ */
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
---
+++ src/GS/cmds/PingCmd.cxx
@@ -0,0 +1,68 @@
+/*                     P I N G C M D . C X X
+ * BRLCAD
+ *
+ * Copyright (c) 2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file PingCmd.cxx
+ * PingCmd.cxx
+ *
+ *  Created on: Dec 22, 2010
+ */
+
+#include "PingCmd.h"
+
+PingCmd::PingCmd(): AbstractClientCmd("ping"){}
+
+PingCmd::~PingCmd() {}
+
+
+QString
+PingCmd::getUsage(){
+	return "Usage: ping";
+}
+
+QString
+PingCmd::getHelp(){
+	return "Pings the remote host.  Pong is expected in return.";
+}
+
+bool
+PingCmd::_exec(GSClient* client, QStringList args){
+	Portal* p = client->getCurrentPortal();
+
+	if (p == NULL) 	{
+		this->log->logERROR("LoginCmd", "Current Portal is NULL.");
+		return false;
+	}
+
+	quint64 now = Logger::getCurrentTime();
+	PingMsg msg(now);
+
+	p->send(&msg);
+	return true;
+}
+
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
