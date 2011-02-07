ac801721fae7964e513776cff915d1118677037e
David Loman <dloman77@gmail.com>
2011-02-07 14:47:24 +0000

    Continuing year 2010 to 2011 text updates.

--- src/adminpanel/ACPChatterBox.h
+++ src/adminpanel/ACPChatterBox.h
@@ -1,7 +1,7 @@
 /*             A C P C H A T T E R B O X . H
  * BRL-CAD
  *
- * Copyright (c) 2010 United States Government as represented by
+ * Copyright (c) 2011 United States Government as represented by
  * the U.S. Army Research Laboratory.
  *
  * This library is free software; you can redistribute it and/or
--- src/adminpanel/AdminControlPanel.h
+++ src/adminpanel/AdminControlPanel.h
@@ -1,7 +1,7 @@
 /*             A D M I N C O N T R O L P A N E L . H
  * BRL-CAD
  *
- * Copyright (c) 2010 United States Government as represented by
+ * Copyright (c) 2011 United States Government as represented by
  * the U.S. Army Research Laboratory.
  *
  * This library is free software; you can redistribute it and/or
